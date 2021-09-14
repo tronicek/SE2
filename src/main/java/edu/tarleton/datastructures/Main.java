32627e7e1575e98089d2c031f24061f02be32a9b
Zdenek Tronicek <tronicek@tarleton.edu>
2021-09-14 08:42:17 -0500

    main method modified

--- src/main/java/edu/tarleton/datastructures/Main.java
+++ src/main/java/edu/tarleton/datastructures/Main.java
@@ -9,14 +9,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Queue queue = new Queue(5);
+        Queue queue = new Queue(100);
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         while (m != 0) {
-            if (queue.isFull()) {
-                int p = queue.get();
-                System.out.printf("Queue is full: %d%n", p);
-            }
             queue.append(m);
             m = sc.nextInt();
         }
